53913f804e0ff36e2a2e96916be7b5ebc1078f1b
lmamc <lmamc@DESKTOP-TU5V1CH>
2022-05-14 19:12:43 -0500

    Clases morfologicamente creadas

---
+++ src/Main/ManejadorCitas.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+import java.util.ArrayList;
+/**
+ *
+ * @author lmamc
+ */
+public class ManejadorCitas {
+    private String pathCitas;
+    
+    
+    public static int leerArchivoCitas() {
+    return 2;};
+    public static  void mostrarCitaEliminada(){};
+    public static void mostrarCitaAgendada(){};
+    public static void reservarCita(){};
+    public static void eliminarCita(){};
+    public static void sobreEscribirArchivo(){};
+    
+    
+    
+}
